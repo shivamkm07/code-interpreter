fdc1d4ed57c40742c70a3c2b61d88ca55a95d621
Shivam Kumar <shivamkm07@gmail.com>
2024-02-29 11:53:36 +0530

    wip

--- tests/perf/test.js
+++ tests/perf/test.js
@@ -1,7 +1,10 @@
 import http from 'k6/http';
 import exec from 'k6/execution';
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
+import { jUnit } from 'https://jslib.k6.io/k6-summary/0.0.2/index.js';
 import { check } from 'k6';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
 export const options = {
     discardResponseBodies: true,
     thresholds: {
@@ -42,12 +45,46 @@ export default function () {
     })
 }
 
+function addTrendMetrics(metrics, prefix, values) {
+  metrics.push([prefix + 'MIN', values.min.toFixed(2) + ' ms']);
+  metrics.push([prefix + 'MAX', values.max.toFixed(2) + ' ms']);
+  metrics.push([prefix + 'MED', values.med.toFixed(2) + ' ms']);
+  metrics.push([prefix + 'AVG', values.avg.toFixed(2) + ' ms']);
+  metrics.push([prefix + 'P95', values['p(95)'].toFixed(2) + ' ms']);
+  metrics.push([prefix + 'P90', values['p(90)'].toFixed(2) + ' ms']);
+}
+
+function addCounterMetrics(metrics, prefix, values) {
+  metrics.push([prefix + 'COUNT', values.count.toString()]);
+  metrics.push([prefix + 'RATE', values.rate.toFixed(2)]);
+}
+
+function addRateMetrics(metrics, prefix, values) {
+  metrics.push([prefix + 'PASSED', values.passes.toString()]);
+  metrics.push([prefix + 'FAILED', values.fails.toString()]);
+}
+
+function addMaxGaugeMetrics(metrics, prefix, values) {
+  metrics.push([prefix + 'MAX', values.max.toString()]);
+}
+
+function extractMetrics(data) {
+    let metrics = [];
+    addMaxGaugeMetrics(metrics, 'VUs ', data.metrics.vus);
+    addCounterMetrics(metrics, 'Iterations ' ,data.metrics.iterations);
+    addRateMetrics(metrics, 'Checks ', data.metrics.checks);
+    addTrendMetrics(metrics, 'Req Duration ', data.metrics.http_req_duration.values);
+    addTrendMetrics(metrics, 'Req Waiting ', data.metrics.http_req_waiting.values);
+    return metrics;
+}
+
 export function handleSummary(data) {
-    // for (const key in data.metrics) {
-    //   if (key.startsWith('data_')) delete data.metrics[key];
-    // }
-  
+    metrics = extractMetrics(data);
     return {
       stdout: textSummary(data, {enableColors: true }),
+      'test_perf_summary.json': JSON.stringify(data),
+      'test_perf_summary.xml': jUnit(data),
+      'test_perf_metrics.json': JSON.stringify(metrics),
+      "test_perf_summary.html": htmlReport(data),
     };
 }
